16350ddf5560f6025c2b675966b954eeb8536e13
Shumerez <52412906+Shumerez@users.noreply.github.com>
2023-05-18 08:53:58 +0300

    Update index.js
    added some imports or whatever for ru translation

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,10 +2,12 @@ import enUS from './en-US'
 import zhCN from './zh-CN'
 import deDE from './de-DE'
 import frFR from './fr-FR'
+import ruRU from './ru-RU'
 
 export default {
   'en-US': enUS,
   'zh-CN': zhCN,
   'de-DE': deDE,
-  'fr-FR': frFR
+  'fr-FR': frFR,
+  'ru-RU': ruRU
 } 
